510e67931b330f2e444a9821d1bafd3c9b709222
JS <49812749+JS00001@users.noreply.github.com>
2022-05-15 14:17:37 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-// add
+// adda
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
