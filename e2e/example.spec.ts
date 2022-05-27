a908ef673fc432aee6078fe34a2687a8721c4401
JS <49812749+JS00001@users.noreply.github.com>
2022-05-26 20:28:36 -0400

    Testing Setup, Builds should pass

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,1 +1,14 @@
 import { test, expect, Page } from '@playwright/test';
+
+test.describe('Landing page', () => {
+	test.beforeEach(async ({ page, baseURL }) => {
+		if (typeof baseURL !== 'undefined') {
+			await page.goto(baseURL);
+		}
+	});
+
+	test('Renders landing title', async ({ page }) => {
+		const title = await page.title();
+		expect(title).toBe('Alpine Client');
+	});
+});
